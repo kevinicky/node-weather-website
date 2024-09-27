76438e9f2009fd5af6008dfcc040e88a942e2efc
Kevin Nicky Setiawan <kevinicky@proton.me>
2024-09-27 10:20:53 +0700

    fix: change postman-request into axios due deprecated

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,25 +1,26 @@
-const request = require('postman-request')
+const axios = require('axios')
 
-const forecast = (latitude, longitude, callback) => {
+const forecast = async (latitude, longitude, callback) => {
 
     const url = 'http://api.weatherstack.com/current?access_key=bc9428fddb73c834e2c86c73955cefbb&query=' + latitude + ',' + longitude
 
-    request({ url, json: true }, (err, { body } = {}) => {
-        if (err) {
-            callback('Unable to connect to weather service!', undefined)
-        }
-        else if (body.error) {
+    try {
+        const response = await axios.get(url)
+        const body = response.data
+
+        if (body.error) {
             callback('Unable to find location!', undefined)
         }
-        else {
-            const temperature = body.current.temperature
-            const feelslike = body.current.feelslike
-            const forecast = body.current.weather_descriptions[0]
-            const humidity = body.current.humidity
 
-            callback(undefined, forecast + '. It is currently ' + temperature + ' degress out. It feels like ' + feelslike + ' degrees out. The humidity is ' + humidity + '%')
-        }
-    })
+        const temperature = body.current.temperature
+        const feelslike = body.current.feelslike
+        const forecast = body.current.weather_descriptions[0]
+        const humidity = body.current.humidity
+
+        callback(undefined, forecast + '. It is currently ' + temperature + ' degress out. It feels like ' + feelslike + ' degrees out. The humidity is ' + humidity + '%')
+    } catch (error) {
+        callback('Unable to connect to weather service!', undefined)
+    }
 }
 
 module.exports = forecast
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,13 +1,13 @@
-const request = require('postman-request')
+const axios = require('axios')
 
-const geocode = (address, callback) => {
+const geocode = async (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' +  encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoia2V2aW5uaWNreSIsImEiOiJjazl3bnhrcnAwM2IwM2dsaXdjNHcxeGdoIn0.vqUypMzSSkr_CbvTHiSi0Q&limit=1'
 
-    request({ url: url, json: true }, (err, { body } = {}) => {
-        if (err) {
-            callback('Unable to connect to location services!', undefined)
-        }
-        else if (body.features.length === 0) {
+    try {
+        const response = await axios.get(url)
+        const body = response.data
+
+        if (body.features.length === 0) {
             callback('Unable to find location. Try another search', undefined)
         }
         else {
@@ -17,7 +17,9 @@ const geocode = (address, callback) => {
                 location: body.features[0].place_name
             })
         }
-    })
+    } catch (error) {
+        callback('Unable to connect to location services!', undefined)
+    }
 }
 
 module.exports = geocode
